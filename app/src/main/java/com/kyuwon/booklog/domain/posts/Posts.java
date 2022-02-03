4eb6bc9eaa8bbd56596e511644f06cf13a4c6434
kyuwon53 <lkw2v@naver.com>
2022-02-03 22:59:37 +0900

    [Create] 게시물 객체를 생성하라 게시물 id, 제목, 내용, 작성자 컬럼 생성

---
+++ app/src/main/java/com/kyuwon/booklog/domain/posts/Posts.java
@@ -0,0 +1,52 @@
+package com.kyuwon.booklog.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * 게시물 정보를 저장하는 객체
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+    /**
+     * 게시물 아이디
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 게시물 제목
+     */
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    /**
+     * 게시물 내용
+     */
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    /**
+     * 게시물 작성자
+      */
+    @Column(nullable = false)
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
+
