8e98f20158bf2bc57b3920c979a7f6c914db857e
kyuwon53 <lkw2v@naver.com>
2022-02-04 22:30:16 +0900

    [Add] JPA Auditing 애너테이션을 추가

--- app/src/main/java/com/kyuwon/booklog/App.java
+++ app/src/main/java/com/kyuwon/booklog/App.java
@@ -5,7 +5,9 @@ package com.kyuwon.booklog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class App {
     public String getGreeting() {
