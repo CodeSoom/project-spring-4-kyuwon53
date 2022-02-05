29a74743c97bcaea83d76b1d52154de91fe667b5
kyuwon53 <lkw2v@naver.com>
2022-02-05 23:15:05 +0900

    [Create] 게시물 요청 정보를 담는 객체를 생성하라 Entity 클래스를 Request 클래스와 분리 view Layer와 DB Layer 분리

---
+++ app/src/main/java/com/kyuwon/booklog/dto/posts/PostsSaveRequestData.java
@@ -0,0 +1,29 @@
+package com.kyuwon.booklog.dto.posts;
+
+import com.kyuwon.booklog.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestData {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestData(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
