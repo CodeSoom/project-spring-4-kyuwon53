5dc2261ebea9da7facf0ad8450d0d46524e0e046
kyuwon53 <lkw2v@naver.com>
2022-02-06 23:02:11 +0900

    [Add] 게시물 업데이트 메소드를 추가하라 게시물은 제목과 내용을 수정할 수 있다.

--- app/src/main/java/com/kyuwon/booklog/domain/posts/Posts.java
+++ app/src/main/java/com/kyuwon/booklog/domain/posts/Posts.java
@@ -48,5 +48,10 @@ public class Posts extends BaseTimeEntity {
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
 
