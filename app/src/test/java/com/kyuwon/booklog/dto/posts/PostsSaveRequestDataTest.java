b87c9351766d319fe64a31dfa57a26fbcc6cf15a
kyuwon53 <lkw2v@naver.com>
2022-02-06 15:40:57 +0900

    [Add] NotBlank 애너테이션이 제대로 작동하는지 테스트하라 validation이 제대로 작동하는지 검사

--- app/src/test/java/com/kyuwon/booklog/dto/posts/PostsSaveRequestDataTest.java
+++ app/src/test/java/com/kyuwon/booklog/dto/posts/PostsSaveRequestDataTest.java
@@ -1,23 +1,68 @@
 package com.kyuwon.booklog.dto.posts;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("게시물 요청 데이터는")
 class PostsSaveRequestDataTest {
+    private Validator validator;
+    private static ValidatorFactory validatorFactory;
 
-    @Test
-    public void it_return_post() {
+    @BeforeEach
+    void setUp() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @Nested
+    @DisplayName("제목, 내용, 작성자가 빈 값이 아니면")
+    class Context_with_not_blank {
         String title = "title";
         String content = "content";
         String author = "author";
 
-        PostsSaveRequestData requestData = new PostsSaveRequestData(title, content, author);
+        @Test
+        @DisplayName("제목, 내용, 작성자가 저장된다.")
+        public void it_return_post() {
+            PostsSaveRequestData requestData = new PostsSaveRequestData(title, content, author);
 
-        assertThat(requestData.getTitle()).isEqualTo(title);
-        assertThat(requestData.getContent()).isEqualTo(content);
-        assertThat(requestData.getAuthor()).isEqualTo(author);
+            assertThat(requestData.getTitle()).isEqualTo(title);
+            assertThat(requestData.getContent()).isEqualTo(content);
+            assertThat(requestData.getAuthor()).isEqualTo(author);
+        }
+    }
+
+    @Nested
+    @DisplayName("제목, 내용, 작성자에 null, \"\",\" \" 값이 들어간다면")
+    class Context_without_title {
+        PostsSaveRequestData requestData;
+
+        @BeforeEach
+        void setUp() {
+            requestData = PostsSaveRequestData.builder()
+                    .title(null)
+                    .content("")
+                    .author(" ")
+                    .build();
+        }
+
+        @Test
+        @DisplayName("validator이 값을 가진다.")
+        public void it_validation() {
+            Set<ConstraintViolation<PostsSaveRequestData>> violations = validator.validate(requestData);
+
+            assertThat(violations).isNotEmpty();
+        }
     }
 }
