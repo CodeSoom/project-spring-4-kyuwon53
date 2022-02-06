3233737e16362ff650efae9f82fea470a35173a8
kyuwon53 <lkw2v@naver.com>
2022-02-06 23:01:11 +0900

    [Add] 게시물 데이터가 수정되는지 테스트하라 posts update 메소드가 제대로 작동하는지 테스트

--- app/src/test/java/com/kyuwon/booklog/domain/posts/PostsTest.java
+++ app/src/test/java/com/kyuwon/booklog/domain/posts/PostsTest.java
@@ -1,7 +1,49 @@
 package com.kyuwon.booklog.domain.posts;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("게시물 모델")
 class PostsTest {
+    private static final String TITLE = "테스트 제목";
+    private static final String CONTENT = "테스트 내용";
+    private static final String AUTHOR = "테스트 작성자";
+    private static final String NEW_TITLE = "새로운 제목";
+    private static final String NEW_CONTENT = "새로운 내용";
 
+    @Nested
+    @DisplayName("게시물 수정시")
+    class Describe_update {
+        Posts posts;
+
+        @BeforeEach
+        void setUp() {
+            posts = Posts.builder()
+                    .title(TITLE)
+                    .content(CONTENT)
+                    .author(AUTHOR)
+                    .build();
+        }
+
+        @Nested
+        @DisplayName("제목,내용이 주어지면")
+        class Context_with_title_content {
+            @BeforeEach
+            void update() {
+              posts.update(NEW_TITLE, NEW_CONTENT);
+            }
+
+            @Test
+            @DisplayName("변경된 제목, 내용을 리턴한다.")
+            void it_return_change() {
+                assertThat(posts.getTitle()).isEqualTo(NEW_TITLE);
+                assertThat(posts.getContent()).isEqualTo(NEW_CONTENT);
+            }
+        }
+    }
 }
