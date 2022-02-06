06a5673729a1a221f16f417f1bd0cf71209dd320
kyuwon53 <lkw2v@naver.com>
2022-02-06 15:41:44 +0900

    [Add] validation 설정 제목, 내용, 작성자는 빈값이 될 수 없다. notBlank (null, "", " ") 모두 잡을수 있는 애너테이션 적용

--- app/src/main/java/com/kyuwon/booklog/dto/posts/PostsSaveRequestData.java
+++ app/src/main/java/com/kyuwon/booklog/dto/posts/PostsSaveRequestData.java
@@ -5,11 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class PostsSaveRequestData {
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String content;
+
+    @NotBlank
     private String author;
 
     @Builder
