46f25cfa498bf75dbfdb934ec5f712207b2a1085
kyuwon53 <lkw2v@naver.com>
2022-02-06 23:31:40 +0900

    [Create] 게시물 수정 모델을 만들어라

---
+++ app/src/main/java/com/kyuwon/booklog/dto/posts/PostsUpdateRequestData.java
@@ -0,0 +1,22 @@
+package com.kyuwon.booklog.dto.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 게시물 수정 모델
+ * 게시물 수정은 제목과 내용만 가능하다.
+ */
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestData {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestData(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
