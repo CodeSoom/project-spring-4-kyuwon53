d1e42ccd43eb2aaa9fdb380577ad5c7775099c04
kyuwon53 <lkw2v@naver.com>
2022-02-14 23:09:17 +0900

    [Create] id를 인코딩된 token으로 변환하는 기능을 구현하라

---
+++ app/src/main/java/com/kyuwon/booklog/utils/WebTokenUtil.java
@@ -0,0 +1,34 @@
+package com.kyuwon.booklog.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+/**
+ * 토큰에 관한 기능을 제공한다.
+ */
+@Component
+public class WebTokenUtil {
+    private final Key key;
+
+    public WebTokenUtil(@Value("${jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    /**
+     * id를 인코딩된 token으로 변환해 리턴한다.
+     *
+     * @param id 유저 id
+     * @return 인코딩된 token
+     */
+    public String encode(Long id) {
+
+        return Jwts.builder()
+                .claim("userId", id)
+                .signWith(key)
+                .compact();
+    }
+}
