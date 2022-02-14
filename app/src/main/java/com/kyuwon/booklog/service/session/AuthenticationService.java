7516c89b5f945a855da6a508fafd43b172cbd256
kyuwon53 <lkw2v@naver.com>
2022-02-15 00:01:45 +0900

    [Refactoring] 사용자 이메일을 검사하고 인증 토큰을 발급하라 사용자 이메일이 존재하는지 검사하는 로직 추가. 하드코딩 되어있던 사용자 아이디 인코딩을 변수로 바꿈.

--- app/src/main/java/com/kyuwon/booklog/service/session/AuthenticationService.java
+++ app/src/main/java/com/kyuwon/booklog/service/session/AuthenticationService.java
@@ -1,5 +1,9 @@
 package com.kyuwon.booklog.service.session;
 
+import com.kyuwon.booklog.domain.user.User;
+import com.kyuwon.booklog.domain.user.UserRepository;
+import com.kyuwon.booklog.dto.user.UserLoginData;
+import com.kyuwon.booklog.errors.LoginFailException;
 import com.kyuwon.booklog.utils.WebTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +14,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class AuthenticationService {
-    private final WebTokenUtil jwtUtil;
+    private final WebTokenUtil webTokenUtil;
+    private final UserRepository userRepository;
 
-    public String login() {
+    /**
+     * 사용자 이메일이 존재한다면 token을 리턴하고 존재하지 않다면 로그인이 실패했다는 예외를 던진다.
+     *
+     * @param userLoginData 사용자 로그인 정보
+     * @return 인증 토큰
+     * @throws LoginFailException 로그인 이메일을 찾을 수 없는 경우
+     */
+    public String login(UserLoginData userLoginData) {
+        String userEmail = userLoginData.getEmail();
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new LoginFailException(userEmail));
 
-        return jwtUtil.encode(1L);
+        return webTokenUtil.encode(user.getId());
     }
 }
