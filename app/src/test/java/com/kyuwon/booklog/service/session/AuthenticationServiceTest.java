174268a6d620e76a41f42cab4fd253a36b216e7b
kyuwon53 <lkw2v@naver.com>
2022-02-14 23:59:16 +0900

    [Refactoring] 로그인시 토큰 인증 테스트를 리팩토링하라 회원가입후 존재하는 이메일에 대한 테스트를 작성

--- app/src/test/java/com/kyuwon/booklog/service/session/AuthenticationServiceTest.java
+++ app/src/test/java/com/kyuwon/booklog/service/session/AuthenticationServiceTest.java
@@ -1,5 +1,11 @@
 package com.kyuwon.booklog.service.session;
 
+import com.kyuwon.booklog.domain.user.Role;
+import com.kyuwon.booklog.domain.user.User;
+import com.kyuwon.booklog.domain.user.UserRepository;
+import com.kyuwon.booklog.dto.user.UserLoginData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,8 +19,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AuthenticationServiceTest {
     @Autowired
     private AuthenticationService authenticationService;
+    @Autowired
+    private UserRepository userRepository;
 
     private static final String TOKEN_REGEX = "^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$";
+    private static final String EMAIL = "test@test.com";
+    private static final String PASSWORD = "testpassword123*";
+
+    @AfterEach
+    void cleanUp() {
+        userRepository.deleteAll();
+    }
 
     @Nested
     @DisplayName("로그인은")
@@ -22,10 +37,34 @@ class AuthenticationServiceTest {
         @Nested
         @DisplayName("유효한 정보로 요청하면")
         class Context_with {
+            UserLoginData userLoginData;
+            User user;
+
+            @BeforeEach
+            void signUpUser() {
+                user = User.builder()
+                        .email(EMAIL)
+                        .name("테스트")
+                        .password(PASSWORD)
+                        .picture("test")
+                        .role(Role.USER)
+                        .build();
+
+                userRepository.save(user);
+            }
+
+            @BeforeEach
+            void setUp() {
+                userLoginData = UserLoginData.builder()
+                        .email(user.getEmail())
+                        .password(user.getPassword())
+                        .build();
+            }
+
             @Test
             @DisplayName("토큰을 리턴한다.")
             void it_return_access_Token() {
-                String accessToken = authenticationService.login();
+                String accessToken = authenticationService.login(userLoginData);
 
                 assertThat(accessToken).matches(TOKEN_REGEX);
             }
