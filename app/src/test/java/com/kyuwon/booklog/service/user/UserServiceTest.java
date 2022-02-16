67caca943c24bd47248c218565b7fc320e15cab4
kyuwon53 <lkw2v@naver.com>
2022-02-16 23:13:24 +0900

    [추가] 사용자 정보 수정 테스트를 작성하라 존재하는 이메일이면 수정된다.

--- app/src/test/java/com/kyuwon/booklog/service/user/UserServiceTest.java
+++ app/src/test/java/com/kyuwon/booklog/service/user/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.kyuwon.booklog.service.user;
 
 import com.kyuwon.booklog.domain.user.User;
 import com.kyuwon.booklog.domain.user.UserRepository;
+import com.kyuwon.booklog.dto.user.UserData;
 import com.kyuwon.booklog.dto.user.UserSaveRequestData;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,8 +11,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,8 +19,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserServiceTest {
     private static final String EMAIL = "test@gmail.com";
     private static final String NAME = "테스트이름";
-    private static final String PASSWORD = "testpassword";
+    private static final String PASSWORD = "*testpassword123";
     private static final String PICTURE = "테스트사진";
+
+    private static final String NEW_NAME = "NEW테스트이름";
+    private static final String NEW_PASSWORD = "*newpassword123";
+    private static final String NEW_PICTURE = "NEW테스트사진";
 
     @Autowired
     private UserService userService;
@@ -63,5 +66,54 @@ class UserServiceTest {
                 assertThat(user.getPicture()).isEqualTo(PICTURE);
             }
         }
+    }
+
+    @Nested
+    @DisplayName("회원 정보 수정은")
+    class Discribe_update {
+        User user;
+
+        @BeforeEach
+        void prepareUser() {
+            user = preparedUser(getUser());
+        }
+
+        @Nested
+        @DisplayName("본인 정보를 수정하면")
+        class Context_when_match_id {
+            UserData userModifyData;
+
+            @BeforeEach
+            void setUp() {
+                userModifyData = UserData.builder()
+                        .name(NEW_NAME)
+                        .password(NEW_PASSWORD)
+                        .picture(NEW_PICTURE)
+                        .build();
+            }
+
+            @Test
+            @DisplayName("수정하고 수정된 정보를 리턴한다.")
+            void it_return_update_user() {
+                String email = user.getEmail();
+                User updateUser = userService.updateUser(email, userModifyData);
+
+                assertThat(updateUser.getName()).isEqualTo(NEW_NAME);
+                assertThat(updateUser.getPicture()).isEqualTo(NEW_PICTURE);
+            }
+        }
+    }
+
+    private UserSaveRequestData getUser() {
+        return UserSaveRequestData.builder()
+                .email(EMAIL)
+                .name(NAME)
+                .password(PASSWORD)
+                .picture(PICTURE)
+                .build();
+    }
+
+    private User preparedUser(UserSaveRequestData saveRequestData) {
+        return userService.signUp(saveRequestData);
     }
 }
