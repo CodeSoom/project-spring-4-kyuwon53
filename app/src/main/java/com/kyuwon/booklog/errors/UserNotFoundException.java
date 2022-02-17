ab678f06cd0105ea7aa50bd67d8440dabbac1e71
kyuwon53 <lkw2v@naver.com>
2022-02-17 23:37:00 +0900

    [추가] id에 해당하는 사용자를 찾을 수 없다는 예외 생성자 추가

--- app/src/main/java/com/kyuwon/booklog/errors/UserNotFoundException.java
+++ app/src/main/java/com/kyuwon/booklog/errors/UserNotFoundException.java
@@ -1,7 +1,11 @@
 package com.kyuwon.booklog.errors;
 
-public class UserNotFoundException extends RuntimeException{
+public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String email) {
-        super("해당 이메일의 사용자를 찾을 수 없습니다. email: "+ email);
+        super("해당 이메일의 사용자를 찾을 수 없습니다. email: " + email);
+    }
+
+    public UserNotFoundException(Long id) {
+        super("해당 아이디의 사용자를 찾을 수 없습니다. id: " + id);
     }
 }
