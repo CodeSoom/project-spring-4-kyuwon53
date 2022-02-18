a0aa17b602afbda97791cc0423070791f8576149
kyuwon53 <lkw2v@naver.com>
2022-02-18 20:31:15 +0900

    [작성] 사용자 수정요청 테스트를 작성하라 사용자가 존재할 때 수정요청 케이스를 작성

--- app/src/test/java/com/kyuwon/booklog/controller/user/UserControllerTest.java
+++ app/src/test/java/com/kyuwon/booklog/controller/user/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kyuwon.booklog.domain.user.User;
 import com.kyuwon.booklog.domain.user.UserRepository;
+import com.kyuwon.booklog.dto.user.UserData;
 import com.kyuwon.booklog.dto.user.UserSaveRequestData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,8 +21,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -32,6 +36,10 @@ class UserControllerTest {
     private static final String EMAIL = "test@email.com";
     private static final String PASSWORD = "1234abcd*";
     private static final String PICTURE = "테스트 사진";
+
+    private static final String NEW_NAME = "NEW테스트 이름";
+    private static final String NEW_PASSWORD = "NEW1234abcd*";
+    private static final String NEW_PICTURE = "NEW테스트 사진";
 
     @Autowired
     private MockMvc mockMvc;
@@ -77,6 +85,48 @@ class UserControllerTest {
                         .andDo(print());
             }
         }
+    }
+
+    @Nested
+    @DisplayName("사용자 수정 요청은")
+    class Describe_update {
+        UserData userUpdatedData;
+        User user;
+
+        @BeforeEach
+        void createUser() throws Exception {
+            user = prepareUser(getUserSaveData());
+        }
+
+        @Nested
+        @DisplayName("존재하는 사용자일 경우")
+        class Context_when_exist_user {
+            @BeforeEach
+            void setUp() {
+                userUpdatedData = getModifyUserData(user.getEmail());
+            }
+
+            @Test
+            @DisplayName("정보를 수정하고 OK를 응답한다.")
+            void it_response_status_ok() throws Exception {
+                mockMvc.perform(patch("/users/" + user.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(userUpdatedData)))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.name", is(NEW_NAME)))
+                        .andExpect(jsonPath("$.email", is(user.getEmail())));
+            }
+        }
+    }
+
+    private UserData getModifyUserData(String email) {
+        return UserData.builder()
+                .email(email)
+                .name(NEW_NAME)
+                .password(NEW_PASSWORD)
+                .picture(NEW_PICTURE)
+                .build();
     }
 
     private UserSaveRequestData getUserSaveData() {
