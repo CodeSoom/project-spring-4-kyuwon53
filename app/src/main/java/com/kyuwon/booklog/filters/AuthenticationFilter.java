32f2ff9ea36e05289b144eb6109bcff64e88bbeb
kyuwon53 <lkw2v@naver.com>
2022-02-22 21:13:23 +0900

    [추가] 인증 처리 기능을 추가하라 인증 토큰을 받아서 정상적인 값이면 사용자에게 일반사용자 권한을 부여하고 아니라면 UNAUTHORIZED를 응답하라

--- app/src/main/java/com/kyuwon/booklog/filters/AuthenticationFilter.java
+++ app/src/main/java/com/kyuwon/booklog/filters/AuthenticationFilter.java
@@ -1,6 +1,13 @@
 package com.kyuwon.booklog.filters;
 
+import com.kyuwon.booklog.domain.user.Role;
+import com.kyuwon.booklog.errors.InvalidTokenException;
+import com.kyuwon.booklog.security.UserAuthentication;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -15,9 +22,43 @@ public class AuthenticationFilter extends BasicAuthenticationFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request
+    protected void doFilterInternal(
+            HttpServletRequest request
             , HttpServletResponse response
-            , FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(request, response);
+            , FilterChain chain
+    ) throws IOException, ServletException {
+        try {
+            String accessToken = parseAuthorizationHeaderFrom(request);
+
+            if (!accessToken.isBlank()) {
+                Authentication authResult = this.getAuthenticationManager().authenticate(
+                        new UserAuthentication(Role.USER, accessToken)
+                );
+                onSuccessfulAuthentication(request, response, authResult);
+            }
+            chain.doFilter(request, response);
+        } catch (InvalidTokenException e) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+        }
+    }
+
+    @Override
+    protected void onSuccessfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authResult
+    ) {
+        authResult.setAuthenticated(true);
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(authResult);
+    }
+
+    private String parseAuthorizationHeaderFrom(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            return "";
+        }
+        return authorization.substring("Bearer".length());
     }
 }
