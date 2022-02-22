585aebb3b6db7f1d50ee70faa22844fc8b17c81f
kyuwon53 <lkw2v@naver.com>
2022-02-22 21:27:01 +0900

    [추가] 인증필터와 인증에러필터를 추가하라

--- app/src/main/java/com/kyuwon/booklog/config/SecurityConfig.java
+++ app/src/main/java/com/kyuwon/booklog/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.kyuwon.booklog.config;
 
+import com.kyuwon.booklog.filters.AuthenticationErrorFilter;
 import com.kyuwon.booklog.filters.AuthenticationFilter;
+import com.kyuwon.booklog.service.session.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.servlet.Filter;
 
@@ -14,14 +20,27 @@ import javax.servlet.Filter;
  * 보안에 관한 설정을 합니다.
  */
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         Filter authenticationFilter = new AuthenticationFilter(
                 authenticationManager()
         );
-        http.csrf().disable()
-                .addFilter(authenticationFilter);
+
+        Filter authenticationErrorFilter = new AuthenticationErrorFilter();
+
+        http
+                .csrf().disable()
+                .addFilter(authenticationFilter)
+                .addFilterBefore(authenticationErrorFilter,
+                        AuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
     }
 
