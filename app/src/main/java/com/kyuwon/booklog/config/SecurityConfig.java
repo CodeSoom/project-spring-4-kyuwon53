0593442580d8dbdf77546dcbe51b1f16a9662390
kyuwon53 <lkw2v@naver.com>
2022-02-22 20:50:41 +0900

    [추가] 보안 설정에 인증필터 추가

--- app/src/main/java/com/kyuwon/booklog/config/SecurityConfig.java
+++ app/src/main/java/com/kyuwon/booklog/config/SecurityConfig.java
@@ -1,16 +1,30 @@
 package com.kyuwon.booklog.config;
 
+import com.kyuwon.booklog.filters.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.Filter;
 
 /**
  * 보안에 관한 설정을 합니다.
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        Filter authenticationFilter = new AuthenticationFilter(
+                authenticationManager()
+        );
+        http.csrf().disable()
+                .addFilter(authenticationFilter);
+
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
