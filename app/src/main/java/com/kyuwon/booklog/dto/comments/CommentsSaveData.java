f2f4790205df6b257cde2eecced2b1739ccccd75
kyuwon53 <lkw2v@naver.com>
2022-02-23 21:42:13 +0900

    [수정] Long 타입에 NotNull로 수정 Long 타입에 NotBlank String 타입이 들어갈 수 없기 때문에 NotNull로 변

--- app/src/main/java/com/kyuwon/booklog/dto/comments/CommentsSaveData.java
+++ app/src/main/java/com/kyuwon/booklog/dto/comments/CommentsSaveData.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * 댓글 저장 데이터 모델
@@ -16,7 +17,7 @@ public class CommentsSaveData {
     /**
      * 댓글을 등록할 게시물 아이디
      */
-    @NotBlank(message = "게시물 아이디는 필수입니다.")
+    @NotNull(message = "게시물 아이디는 필수입니다.")
     private Long postId;
     /**
      * 댓글 내용
