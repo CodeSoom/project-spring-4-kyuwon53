050f5c3c9a529849668b06fe6972abdd294c22e9
kyuwon53 <lkw2v@naver.com>
2022-02-23 21:42:59 +0900

    [추가] validator에 걸리는 테스트 케이스 추가

--- app/src/test/java/com/kyuwon/booklog/dto/comments/CommentsSaveDataTest.java
+++ app/src/test/java/com/kyuwon/booklog/dto/comments/CommentsSaveDataTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,6 +48,40 @@ class CommentsSaveDataTest {
             assertThat(commentsSaveData.getPostId()).isEqualTo(postId);
             assertThat(commentsSaveData.getContent()).isEqualTo(content);
             assertThat(commentsSaveData.getEmail()).isEqualTo(email);
+        }
+    }
+
+    @Nested
+    @DisplayName("필수 입력 값에 빈 값이 들어간다면")
+    class Context_when_blank_data {
+        CommentsSaveData commentsSaveData;
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        @DisplayName("validator에 걸리고 메시지를 출력한다.")
+        void it_validation_return_message(String blank) {
+            commentsSaveData = CommentsSaveData.builder()
+                    .postId(null)
+                    .content(blank)
+                    .email(blank)
+                    .build();
+
+            Set<ConstraintViolation<CommentsSaveData>> violations = validator.validate(commentsSaveData);
+
+            Iterator<ConstraintViolation<CommentsSaveData>> iterator = violations.iterator();
+            List<String> messages = new ArrayList<>();
+
+            while (iterator.hasNext()) {
+                ConstraintViolation<CommentsSaveData> next = iterator.next();
+                messages.add(next.getMessage());
+            }
+
+            assertThat(violations).isNotEmpty();
+            assertThat(messages).contains(
+                    "게시물 아이디는 필수입니다.",
+                    "댓글 내용은 필수입니다.",
+                    "이메일은 필수입니다."
+            );
         }
     }
 }
