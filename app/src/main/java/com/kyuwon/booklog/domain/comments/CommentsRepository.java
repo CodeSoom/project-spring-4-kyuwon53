ca107e508fa882b30172a50a73d5ffa3a6e3d997
kyuwon53 <lkw2v@naver.com>
2022-02-23 21:11:53 +0900

    [작성] 댓글 JPA CRUD 레파지토리를 생성하라

---
+++ app/src/main/java/com/kyuwon/booklog/domain/comments/CommentsRepository.java
@@ -0,0 +1,6 @@
+package com.kyuwon.booklog.domain.comments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentsRepository extends JpaRepository<Comments, Long> {
+}
