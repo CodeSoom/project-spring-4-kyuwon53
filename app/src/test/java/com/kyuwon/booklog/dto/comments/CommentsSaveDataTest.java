2d82c38f43ae3fc2e2b1598aba9a31c45597d718
kyuwon53 <lkw2v@naver.com>
2022-02-23 21:48:56 +0900

    [작성] 이메일 형식 검증 테스트를 작성하라

--- app/src/test/java/com/kyuwon/booklog/dto/comments/CommentsSaveDataTest.java
+++ app/src/test/java/com/kyuwon/booklog/dto/comments/CommentsSaveDataTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -20,6 +21,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("댓글 요청 데이터는")
 class CommentsSaveDataTest {
+    private static final Long POST_ID = 1L;
+    private String CONTENT = "content";
+    private String EMAIL = "email@test.com";
+
     private Validator validator;
     private static ValidatorFactory validatorFactory;
 
@@ -32,29 +37,25 @@ class CommentsSaveDataTest {
     @Nested
     @DisplayName("필수 입력 값이 들어오면")
     class Context_with_Not_blank_comment {
-        Long postId = 1L;
-        String content = "content";
-        String email = "email@test.com";
-
         @Test
         @DisplayName("필수 입력 값이 저장된다.")
         public void it_return_comment() {
             CommentsSaveData commentsSaveData = CommentsSaveData.builder()
-                    .postId(postId)
-                    .content(content)
-                    .email(email)
+                    .postId(POST_ID)
+                    .content(CONTENT)
+                    .email(EMAIL)
                     .build();
 
-            assertThat(commentsSaveData.getPostId()).isEqualTo(postId);
-            assertThat(commentsSaveData.getContent()).isEqualTo(content);
-            assertThat(commentsSaveData.getEmail()).isEqualTo(email);
+            assertThat(commentsSaveData.getPostId()).isEqualTo(POST_ID);
+            assertThat(commentsSaveData.getContent()).isEqualTo(CONTENT);
+            assertThat(commentsSaveData.getEmail()).isEqualTo(EMAIL);
         }
     }
 
     @Nested
     @DisplayName("필수 입력 값에 빈 값이 들어간다면")
     class Context_when_blank_data {
-        CommentsSaveData commentsSaveData;
+        private CommentsSaveData commentsSaveData;
 
         @ParameterizedTest
         @NullAndEmptySource
@@ -82,6 +83,36 @@ class CommentsSaveDataTest {
                     "댓글 내용은 필수입니다.",
                     "이메일은 필수입니다."
             );
+        }
+    }
+
+    @Nested
+    @DisplayName("이메일 형식에 맞지 않는 값이 들어온다면")
+    class Context_when_wrong_email {
+        private CommentsSaveData commentsSaveData;
+
+        @DisplayName("이메일 형식이 잘못됐다는 메세지가 나온다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"asdfga", "asf@", "@sf", "sfa.com", "@safa.com"})
+        void it_return_wrong_email_pattern(String email) {
+            commentsSaveData = CommentsSaveData.builder()
+                    .postId(POST_ID)
+                    .email(email)
+                    .content(CONTENT)
+                    .build();
+
+            Set<ConstraintViolation<CommentsSaveData>> violations = validator.validate(commentsSaveData);
+
+            Iterator<ConstraintViolation<CommentsSaveData>> iterator = violations.iterator();
+            List<String> messages = new ArrayList<>();
+
+            while (iterator.hasNext()) {
+                ConstraintViolation<CommentsSaveData> next = iterator.next();
+                messages.add(next.getMessage());
+            }
+
+            assertThat(violations).isNotEmpty();
+            assertThat(messages).contains("이메일 형식이 아닙니다.");
         }
     }
 }
