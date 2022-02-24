f4cb04753577f39b3ad775b083111bfb1fe4078b
kyuwon53 <lkw2v@naver.com>
2022-02-25 00:53:59 +0900

    [작성] 게시물에 해당하는 댓글 리스트를 테스트하라

--- app/src/test/java/com/kyuwon/booklog/controller/comments/CommentsControllerTest.java
+++ app/src/test/java/com/kyuwon/booklog/controller/comments/CommentsControllerTest.java
@@ -2,6 +2,7 @@ package com.kyuwon.booklog.controller.comments;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kyuwon.booklog.domain.comments.Comments;
 import com.kyuwon.booklog.domain.posts.Posts;
 import com.kyuwon.booklog.dto.comments.CommentsSaveData;
 import com.kyuwon.booklog.dto.posts.PostsSaveRequestData;
@@ -15,12 +16,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -79,6 +85,44 @@ class CommentsControllerTest {
                         .andDo(print());
             }
         }
+    }
+
+    @Nested
+    @DisplayName("댓글 목록 조회 요청")
+    class Describe_Get {
+        @Nested
+        @DisplayName("게시물이 존재하고 댓글이 존재하면")
+        class Context_when_exist_post_comments {
+            private static final int COUNT = 10;
+
+            @BeforeEach
+            void setComment() throws Exception {
+                for (int i = 0; i < COUNT; i++) {
+                    prepareComment(getComment(post.getId()));
+                }
+            }
+
+            @Test
+            @DisplayName("댓글 목록을 리턴하고 상태 isOk를 응답한다.")
+            void it_response_isOk() throws Exception {
+                mockMvc.perform(get("/comments/"+post.getId()))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(COMMENT)))
+                        .andDo(print());
+            }
+        }
+    }
+
+    private Comments prepareComment(CommentsSaveData commentsSaveData) throws Exception {
+        ResultActions actions = mockMvc.perform(post("/comments")
+                .content(objectMapper.writeValueAsString(commentsSaveData))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        MvcResult mvcResult = actions.andReturn();
+        ;
+        String content = mvcResult.getResponse().getContentAsString();
+
+        return objectMapper.readValue(content, Comments.class);
     }
 
     private CommentsSaveData getComment(Long id) {
