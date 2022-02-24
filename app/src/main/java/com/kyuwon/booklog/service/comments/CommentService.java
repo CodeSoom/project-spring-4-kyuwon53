c0d32ea4ae0b9f83426715a7eaead3ac7d14efdb
kyuwon53 <lkw2v@naver.com>
2022-02-24 20:37:12 +0900

    [추가] 게시물이 존재하는지 여부를 판단하라

--- app/src/main/java/com/kyuwon/booklog/service/comments/CommentService.java
+++ app/src/main/java/com/kyuwon/booklog/service/comments/CommentService.java
@@ -2,7 +2,9 @@ package com.kyuwon.booklog.service.comments;
 
 import com.kyuwon.booklog.domain.comments.Comments;
 import com.kyuwon.booklog.domain.comments.CommentsRepository;
+import com.kyuwon.booklog.domain.posts.PostsRepository;
 import com.kyuwon.booklog.dto.comments.CommentsSaveData;
+import com.kyuwon.booklog.errors.PostsNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,7 @@ import javax.transaction.Transactional;
 @Transactional
 public class CommentService {
     private final CommentsRepository commentsRepository;
+    private final PostsRepository postsRepository;
 
     /**
      * 댓글을 저장하고 등록된 댓글을 리턴한다.
@@ -24,6 +27,11 @@ public class CommentService {
      * @return 등록된 댓글
      */
     public Comments save(CommentsSaveData commentsSaveData) {
+        Long postId = commentsSaveData.getPostId();
+
+        postsRepository.findById(postId)
+                .orElseThrow(() -> new PostsNotFoundException(postId));
+
         return commentsRepository.save(commentsSaveData.toEntity());
     }
 }
