68d0a4477dcb48fedc1aaebb2ade5616335f9118
kyuwon53 <lkw2v@naver.com>
2022-02-24 20:29:25 +0900

    [작성] 댓글 작성 기능을 테스트하라

---
+++ app/src/test/java/com/kyuwon/booklog/service/comments/CommentServiceTest.java
@@ -0,0 +1,85 @@
+package com.kyuwon.booklog.service.comments;
+
+import com.kyuwon.booklog.domain.comments.Comments;
+import com.kyuwon.booklog.domain.comments.CommentsRepository;
+import com.kyuwon.booklog.domain.posts.Posts;
+import com.kyuwon.booklog.dto.comments.CommentsSaveData;
+import com.kyuwon.booklog.dto.posts.PostsSaveRequestData;
+import com.kyuwon.booklog.service.posts.PostsService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@DisplayName("댓글 관리")
+class CommentServiceTest {
+    private static final String COMMENT_CONTENT = "댓글 내용";
+    private static final String EMAIL = "test@email.com";
+
+    private static final String TITLE = "테스트 제목";
+    private static final String POST_CONTENT = "테스트 내용";
+    private static final String AUTHOR = "테스트 작성자";
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private CommentsRepository commentsRepository;
+
+    private Posts post;
+
+    @BeforeEach
+    public void setPost() {
+        post = postsService.save(getPost());
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        commentsRepository.deleteAll();
+    }
+
+    @Nested
+    @DisplayName("댓글 작성은")
+    class Discribe_save {
+        Comments comment;
+
+        @Nested
+        @DisplayName("댓글을 입력받으면")
+        class Context_when_comment {
+            @Test
+            @DisplayName("저장하고 댓글을 리턴한다.")
+            void it_return_comment() {
+                comment = commentService.save(getComment(post.getId()));
+
+                assertThat(comment.getPostId()).isEqualTo(post.getId());
+                assertThat(comment.getContent()).isEqualTo(COMMENT_CONTENT);
+                assertThat(comment.getEmail()).isEqualTo(EMAIL);
+            }
+        }
+    }
+
+    private CommentsSaveData getComment(Long id) {
+        return CommentsSaveData.builder()
+                .postId(id)
+                .content(COMMENT_CONTENT)
+                .email(EMAIL)
+                .build();
+    }
+
+    private PostsSaveRequestData getPost() {
+        return PostsSaveRequestData.builder()
+                .title(TITLE)
+                .content(POST_CONTENT)
+                .author(AUTHOR)
+                .build();
+    }
+}
