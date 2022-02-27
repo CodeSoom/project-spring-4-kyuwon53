daf197473f797aed1327cd6e286ccad17819e578
kyuwon53 <lkw2v@naver.com>
2022-02-28 01:24:22 +0900

    [추가] 게시물 데이터에 작성자 추가

--- app/src/test/java/com/kyuwon/booklog/dto/posts/PostsSaveRequestDataTest.java
+++ app/src/test/java/com/kyuwon/booklog/dto/posts/PostsSaveRequestDataTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -30,36 +32,35 @@ class PostsSaveRequestDataTest {
         String title = "title";
         String content = "content";
         String author = "author";
+        String email = "test@email.com";
 
         @Test
         @DisplayName("제목, 내용, 작성자가 저장된다.")
         public void it_return_post() {
-            PostsSaveRequestData requestData = new PostsSaveRequestData(title, content, author);
+            PostsSaveRequestData requestData = new PostsSaveRequestData(title, content, author,email);
 
             assertThat(requestData.getTitle()).isEqualTo(title);
             assertThat(requestData.getContent()).isEqualTo(content);
             assertThat(requestData.getAuthor()).isEqualTo(author);
+            assertThat(requestData.getEmail()).isEqualTo(email);
         }
     }
 
     @Nested
     @DisplayName("제목, 내용, 작성자에 빈 값이 들어간다면")
     class Context_without_title {
-        final String[] emptyValue = new String[]{null, "", "  "};
         PostsSaveRequestData requestData;
 
-        @BeforeEach
-        void setUp() {
-            requestData = PostsSaveRequestData.builder()
-                    .title(emptyValue[0])
-                    .content(emptyValue[1])
-                    .author(emptyValue[2])
-                    .build();
-        }
-
-        @Test
+        @ParameterizedTest
+        @NullAndEmptySource
         @DisplayName("validator이 값을 가진다.")
-        public void it_validation() {
+        public void it_validation(String empty) {
+            requestData = PostsSaveRequestData.builder()
+                    .title(empty)
+                    .content(empty)
+                    .author(empty)
+                    .email(empty)
+                    .build();
             Set<ConstraintViolation<PostsSaveRequestData>> violations = validator.validate(requestData);
 
             assertThat(violations).isNotEmpty();
