3bc40ca676f0bbe1787219d4d0f5ba7727b8feb2
kyuwon53 <lkw2v@naver.com>
2022-02-28 23:15:27 +0900

    [추가] 회원가입 폼에 css를 적용시켜라

--- app/src/main/frontend/src/component/user/RegisterPage.jsx
+++ app/src/main/frontend/src/component/user/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import styles from "./Register.module.css";
 
 function RegisterPage() {
   const [name, setName] = useState("");
@@ -8,88 +9,140 @@ function RegisterPage() {
   const [confirmPassword, setConfirmPassword] = useState("")
   const [picture, setPicture] = useState("")
 
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
   const onNameHandler = (event) => {
-    setName(event.currentTarget.value)
+    const nameRegex = /^[가-힣a-zA-Z]{2,}}$/;
+    const nameValue = event.currentTarget.value;
+    const nameValidation = (!nameValue || (nameRegex.test(nameValue)));
+    if (nameValidation) {
+      event.currentTarget.focus();
+    }
+    setNameError(!nameValidation);
+    setName(nameValue);
   }
   const onEmailHandler = (event) => {
-    setEmail(event.currentTarget.value)
+    const emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+    const emailValue = event.currentTarget.value;
+    const emailValidation = (!emailValue || (emailRegex.test(emailValue)));
+
+    setEmailError(!emailValidation);
+    setEmail(emailValue);
   }
   const onPasswordHandler = (event) => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
+    const passwordValue = event.currentTarget.value;
+    const passwordValidation = (!passwordValue || (passwordRegex.test(passwordValue)));
+    const confirmPasswordValidation = (!confirmPassword || passwordValue === confirmPassword);
+
+    setPasswordError(!passwordValidation);
+    setConfirmPassword(!confirmPasswordValidation)
     setPassword(event.currentTarget.value)
   }
   const onConfirmPasswordHandler = (event) => {
+    const confirmPasswordValidation = (password === event.currentTarget.value);
+
+    setConfirmPasswordError(!confirmPasswordValidation);
     setConfirmPassword(event.currentTarget.value)
   }
   const onPictureHandler = (event) => {
     setPicture(event.currentTarget.files)
   }
+  const validation = () => {
+    if (!name) {
+      setNameError(true);
+    }
+    if (!password) {
+      setPasswordError(true);
+    }
+    if (!confirmPassword) {
+      setConfirmPasswordError(true);
+    }
+    if (!email) {
+      setEmailError(true);
+    }
+
+    if (name && password && confirmPassword && email) {
+      return true;
+    }
+  }
+
   async function registUser() {
     const response = await axios.post(
       `http://localhost:8080/users`,
       { name, email, password, picture }
-    ).catch((error)=>{
+    ).catch((error) => {
       console.log(error);
     })
     console.log(response.data);
     return response.data;
   }
+
   const onSubmit = (event) => {
     event.preventDefault()
-    if (password !== confirmPassword) {
-      console.log(password, confirmPassword, (password === confirmPassword));
-      alert('비밀번호와 비밀번호 확인이 다릅니다.')
+    if (validation()) {
       return
     }
     registUser();
   }
+
   return (
-    <div>
+    <div className={styles.container}>
       <form>
-        <div>
-          <input
-            name="name"
-            type="text"
-            placeholder="이름을 입력하세요"
-            value={name}
-            onChange={onNameHandler}
-          />
+        <div className={styles.row}>
+          <div className={styles.inputGroup}>
+            <input
+              name="name"
+              type="text"
+              placeholder="이름을 입력하세요"
+              value={name}
+              onChange={onNameHandler}
+            />
+            {nameError && <div className={styles.invalid}>이름은 2글자 이상입력하세요</div>}
+          </div>
+          <div className={styles.inputGroup}>
+            <input
+              name="email"
+              type="email"
+              placeholder="이메일을 입력하세요"
+              value={email}
+              onChange={onEmailHandler}
+            />
+            {emailError && <div className={styles.invalid}>이메일 형식에 맞지 않습니다.</div>}
+          </div>
+          <div className={styles.inputGroup}>
+            <input
+              name="password"
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+              value={password}
+              onChange={onPasswordHandler}
+            />
+            {passwordError && <div className={styles.invalid}>비밀번호는 영어,숫자,기호 포함 8-20글자입니다.</div>}
+          </div>
+          <div className={styles.inputGroup}>
+            <input
+              name="confirmPassword"
+              type="password"
+              placeholder="비밀번호 확인"
+              value={confirmPassword}
+              onChange={onConfirmPasswordHandler}
+            />
+            {confirmPasswordError && <div className={styles.invalid}>비밀번호가 일치하지 않습니다..</div>}
+          </div>
+          <div className={styles.inputGroup}>
+            <input
+              name="picture"
+              type="file"
+              accept="image/*"
+              onChange={onConfirmPasswordHandler}
+            />
+          </div>
+          <button className={styles.actionButton} type="submit" onClick={onSubmit}>가입하기</button>
         </div>
-        <div>
-          <input
-            name="email"
-            type="email"
-            placeholder="이메일을 입력하세요"
-            value={email}
-            onChange={onEmailHandler}
-          />
-        </div>
-        <div>
-          <input
-            name="password"
-            type="password"
-            placeholder="비밀번호를 입력하세요"
-            value={password}
-            onChange={onPasswordHandler}
-          />
-        </div>
-        <div>
-          <input
-            name="confirmPassword"
-            type="password"
-            placeholder="비밀번호 확인"
-            value={confirmPassword}
-            onChange={onConfirmPasswordHandler}
-          />
-        </div>
-        <div>
-          <input
-            name="picture"
-            type="file"
-            accept="image/*"
-            onChange={onConfirmPasswordHandler}
-          />
-        </div>
-        <button type="submit" onClick={onSubmit}>가입하기</button>
       </form>
     </div>
   );
